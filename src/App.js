c1c946a1fb01da05889f693168ea153624e4681c
Shoxrux Asadov <129892167+ShoxruxAsadov@users.noreply.github.com>
2023-09-06 14:27:15 +0500

    Dummy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "./api/instance";
-import { Pagination, PaginationItem } from "@mui/material";
+import { Pagination } from "@mui/material";
 
 export default function App() {
   const [products, setProducts] = useState([]);
